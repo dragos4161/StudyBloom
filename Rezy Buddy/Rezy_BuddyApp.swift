48c0059830a678911938ed8522eb9ac240a3179a
dragos4161 <dima.dragos0@gmail.com>
2025-11-22 12:54:28 +0200

    Initial Commit

---
+++ Rezy Buddy/Rezy_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rezy_BuddyApp.swift
+//  Rezy Buddy
+//
+//  Created by Dragos Dima on 22.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Rezy_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
